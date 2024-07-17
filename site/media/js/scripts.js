3d426a5e525f04f11b5afe67be1a2173cb80a4ca
Trey Doig <trey@pathlight.com>
2024-07-17 00:09:35 -0700

    Scorch the earth

--- site/media/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() { 
-  $("#sitelogo, #nav>ul>li").hover(function(){
-    $("#doig").animate({"opacity" : 0}, 100);
-  }, function(){
-    $("#doig").animate({"opacity" : 1}, 100);
-  });
-});
